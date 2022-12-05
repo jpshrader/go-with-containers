b9934134e51b140c83bc0afe34542cabe452a6db
Jack Shrader <96661942+jpshrader@users.noreply.github.com>
2022-12-05 02:52:03 -0500

    implement where

--- linq/linq.go
+++
@@ -1,3 +0,0 @@
-package linq
-
-func Where() []
--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"fmt"
 )
 
 func main() {
-	items := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	items := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	evenNumbers
+	evenNumbers := Where(isEven, items)
+
+	fmt.Printf("even numbers: %v", evenNumbers)
+}
+
+func isEven(x int) bool {
+	return x % 2 == 0
+}
+
+type WhereFilter[T any] func(x T) bool
+
+func Where[T any](filter WhereFilter[T], source []T,) ([]T) {
+	returnValues := make([]T, 0)
+
+	for _, item := range source {
+		if filter(item) {
+			returnValues = append(returnValues, item)
+		}
+	}
+
+	return returnValues
 }
